a3a738ee2f06f99aa7ff67ad77facaafed20e24f
Patrick Seidensal <patrick@nawracay.de>
2021-07-19 12:08:04 +0200

    converted ceph-stop to bash script

---
+++ ceph-stop.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+CEPH_DIR=${CEPH_DIR:-~/src/ceph}
+
+cd "${CEPH_DIR}/build"
+../src/stop.sh
+
+# Stop and disable all Ceph related services
+for service in $(
+    find /etc/systemd/system \
+        -regex .\*ceph-.\*@.+.service \
+        -exec basename {} \;
+); do
+    sudo systemctl stop $service
+    sudo systemctl disable $service
+done
+
+# Delete all Ceph-related services
+sudo find /etc/systemd/system \
+    -type d \
+    -name ceph-\*.target.wants \
+    -exec rm -rf {} +
+
+# Reload the systemctl daemon and reset failed services
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
+
+# Remove Ceph directory
+rm -rf /var/lib/ceph
+
+# Stop all podman containers with Ceph in their name
+sudo podman ps --format=json |
+    jq -r '.[] | "\(.Id) \(.Names)"' |
+    grep "${keyword}" |
+    awk -F' ' '{print $1}' |
+    xargs sudo podman stop
